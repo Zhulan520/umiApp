ba0ac6c8a2c1b5a3b454a35f37005d2d4719afdc
二帅 <zes02189043@alibaba-inc.com>
2025-02-06 18:31:05 +0800

    feat: 修改登录

--- src/pages/User/Login/index.tsx
+++ src/pages/User/Login/index.tsx
@@ -126,8 +126,8 @@ const Login: React.FC = () => {
         });
         message.success(defaultLoginSuccessMessage);
         // await fetchUserInfo();
-        const urlParams = new URL(window.location.href).searchParams;
-        history.push(urlParams.get('redirect') || '/');
+        // const urlParams = new URL(window.location.href).searchParams;
+        history.push('/welcome');
         return;
       }
       console.log(msg);

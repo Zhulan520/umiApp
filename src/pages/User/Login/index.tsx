31913bd4b3be3e0e188c81ee53accd64aa50e340
二帅 <zes02189043@alibaba-inc.com>
2025-02-06 18:19:42 +0800

    feat: 修改登录

--- src/pages/User/Login/index.tsx
+++ src/pages/User/Login/index.tsx
@@ -125,7 +125,7 @@ const Login: React.FC = () => {
           defaultMessage: '登录成功！',
         });
         message.success(defaultLoginSuccessMessage);
-        await fetchUserInfo();
+        // await fetchUserInfo();
         const urlParams = new URL(window.location.href).searchParams;
         history.push(urlParams.get('redirect') || '/');
         return;
